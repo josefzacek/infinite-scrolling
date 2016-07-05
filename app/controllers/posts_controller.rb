e5c8df31af5940a536ec9b449f44bbeb8f3e8694
Josef Zacek <info@josefzacek.com>
2016-07-05 22:58:30 +0100

    Post index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.paginate(page: params[:page],
+                           per_page: 12).order('created_at DESC')
   end
 
   def show
