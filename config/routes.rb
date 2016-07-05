ab9611c8bba9c787c3f2c25af8de9a09c16d9486
Josef Zacek <info@josefzacek.com>
2016-07-05 22:32:58 +0100

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/index'
 
-  get 'posts/show'
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
