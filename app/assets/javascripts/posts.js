be6f402210e5c00d61269e6586de0bc40a9be5c8
Josef Zacek <info@josefzacek.com>
2016-07-12 23:14:38 +0100

    js script for load more button

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -11,4 +11,23 @@ $( document ).ready(function() {
     });
   };
 
+
+  //load more posts on click of button
+  if ($('#paginate-load-more-button').size() > 0) {
+    $('#paginate-load-more-button .pagination').hide()
+
+    $('#load-more-posts').show().click(function() {
+      more_posts_url = $('#paginate-load-more-button .pagination .next_page a').attr('href');
+      $this = $(this);
+      $this.html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />').addClass('disabled');
+      $.getScript(more_posts_url, function() {
+        if ($this) {
+          $this.text('Load more posts').removeClass('disabled');
+        }
+      });
+    });
+
+  };
+
 });
+
