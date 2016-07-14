eea75ffbed9465584d4ae4623292d6bc52a65d47
Josef Zacek <info@josefzacek.com>
2016-07-14 23:53:42 +0100

    Load more button set pagination to 12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def load_more_button
     @posts = Post.paginate(page: params[:page],
-                           per_page: 42).order('created_at DESC')
+                           per_page: 12).order('created_at DESC')
 
     respond_to do |format|
       format.html
